f40351a3cc34d4711a399fef83b502f1e82f1d50
hawksolver <samuelvaz161@gmail.com>
2024-11-01 12:05:52 -0300

    newjs

---
+++ js/login.js
@@ -0,0 +1,4 @@
+import Parse from 'parse/react-native.js';
+// Inicialização do Parse com suas credenciais Back4App
+Parse.initialize('njNAqSQm1Bg0LoKu45fURFLlsIPwJ6TFxaCpGLh7', 'RLCspUJ0lSNK8XFYr3r0gU4qyGhQuEkFg81tRZjk');
+Parse.serverURL = 'https://parseapi.back4app.com/';
